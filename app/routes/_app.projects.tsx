4f956c07921c249f115f86848d434cf738b5565a
Callum Kloos <callumkloos@gmail.com>
2024-01-16 17:47:34 +0000

    responsive projects page

--- app/routes/_app.projects.tsx
+++ app/routes/_app.projects.tsx
@@ -7,12 +7,12 @@ export default function ProjectsPage() {
   return (
     <Container>
       <div className="py-4 mb-8">
-        <h1 className="md:leading-14 pb-6 font-syne text-3xl font-extrabold leading-9  tracking-tight text-foreground sm:text-4xl sm:leading-10 md:text-6xl">
+        <h1 className="md:leading-14 pb-6 font-syne text-3xl font-extrabold leading-9 tracking-tight text-foreground sm:text-4xl sm:leading-10 md:text-6xl">
           Projects
         </h1>
         <p className="font-mono">Explore my work.</p>
       </div>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
         {proj.map((project) => (
           <ProjectCard project={project} key={project.name} />
         ))}
