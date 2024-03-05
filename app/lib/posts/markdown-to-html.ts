6c04223b1ce5ad9347c2cfccba3a4680d3e3cbee
Callum Kloos <callumkloos@gmail.com>
2024-03-05 12:03:40 +0000

    disable warning

--- app/lib/posts/markdown-to-html.ts
+++ app/lib/posts/markdown-to-html.ts
@@ -2,7 +2,7 @@ import { unified } from "unified";
 import html from "remark-html";
 import prism from "remark-prism";
 import parse from "remark-parse";
-import remarkGfm from 'remark-gfm';
+import remarkGfm from "remark-gfm";
 import stringify from "remark-stringify";
 
 export async function markdownToHtml(markdown: string) {
@@ -10,6 +10,7 @@ export async function markdownToHtml(markdown: string) {
 		.use(parse)
 		.use(remarkGfm)
 		.use(stringify)
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
 		.use(prism as any, { transformInlineCode: true })
 		.use(html, {
 			sanitize: false,
